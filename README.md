780ec7dd183027011999b18bf25690b25d5f7b9d
kakamorsch <kakamorsch@gmail.com>
2021-01-21 16:14:46 -0300

    finishing the challenge

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # Intructions to run the code
 
-## First step 
+### Dependecies:
 
-Edit the file costumers.js
-By default the file already has a basket of products
+  
+
+**The only dependecy is NodeJS >= 14.15.4**
+
+The above version is recommended for performance reasons and because it is recommended by its developers.
+
+## First step
+
+  
+**Edit the file *distribution.js* or leave as it is.**
+
+By default the file already has a basket of products and a list of users.
+  
 
 ## Second step
 
-Run the npm install or yarn to download the dependencies
-
+  **Inside the *stone* folder, open your terminal and run the command:** `node main.js`
+    
 ## Third step
 
-Run the command:```node main.js```
+You can also tinker on the **main.js** and add the email as a parameter to the function call:
+![enter image description here](https://lh3.googleusercontent.com/pw/ACtC-3d8k5PXmMBlcKNiYCQjf7o-E6nKIRUjsnOEfh5hrulf4JIZ2sTroyh_NmVjm_Xdmpsk-RhBO1-y2sC_S8iPbVHhPsaozhxgg62t5Q1w1rpUzCgS_-TOjeyCXk91bb13HZ_GlODfjxljyHKq6MJVEo0M1Q=w506-h41-no?authuser=0)
+
+**Adding the email as a parameter will bring only the desired email's bill.** 
--- main.js
+++ main.js
@@ -1,41 +1,41 @@
 const distribution = require("./distribution");
-let total = 0; // quanto falta do montante
-let distributed = []; //array contendo os valores distribuidos pelos emails
+let total = 0; // total to be payed
+let distributed = {}; // an object to store the distributed values
 
+// counting the total price to be payed
 const totalBill = products => {
   total = products.reduce((acc, product, idx) => {
     idx === 0 //checks for the first iteration
-      ? (acc =product.price * product.quantity)
+      ? (acc = product.price * product.quantity)
       : (acc = acc + product.price * product.quantity);
     return acc;
-  }, 0)
-  return total
+  }, 0);
+  return total;
 };
 
 const totalToPay = clients => {
-  //cuidado magia negra abaixo
-  clients.contacts.reduce((acc, contact, idx, contacts)=>{
-    let bill
-    if(idx==0){
-      bill = Math.floor(acc / contacts.length)
-      acc = acc - bill 
+  //for each client the total value is decreased
+  clients.contacts.reduce((acc, contact, idx, contacts) => {
+    let bill;
+    if (idx === 0) {
+      bill = Math.floor(acc / contacts.length);
+      acc = acc - bill;
     } else {
-      bill = Math.ceil(acc / (contacts.length - idx))
-      acc = acc - bill
+      bill = Math.ceil(acc / (contacts.length - idx));
+      acc = acc - bill;
     }
-    total = acc 
-    distributed.push({contact, bill})    
-    return acc
-  },totalBill(clients.products))
-  return total
+    total = acc;
+    distributed[contact] = bill;
+    return acc;
+  }, totalBill(clients.products));
+  return distributed;
 };
-
-const distribute = clients => {
+// function that outputs everything or just the desired user
+const distribute = (clients, contact) => {
   if (clients.contacts.length == 0) return "Error, the contact list is empty";
   if (clients.products.length == 0) return "Error, the product list is empty";
-  //returns a list of tuples
-  return totalToPay(clients)
+  if (contact) return totalToPay(clients)[contact];
+  return totalToPay(clients);
 };
 
-console.log(distribute(distribution))
-console.log(distributed)
+console.log(distribute(distribution));
